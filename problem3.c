0e0d1e42ba9bc1d0fd456ee49b6f8f9ab0a0416d
Awani Gupta <awanigupta396@gmail.com>
2026-09-16 01:19:43 +0530

    Add creat system call program

---
+++ problem3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main() {
+    int fd;
+
+    fd = creat("newfile.txt", 0644);
+
+    if (fd == -1) {
+        perror("creat");
+        return 1;
+    }
+
+    printf("File created successfully.\n");
+    printf("File Descriptor: %d\n", fd);
+
+    close(fd);
+
+    return 0;
+}
+
