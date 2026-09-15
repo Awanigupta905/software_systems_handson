4a64511228bea5ccdf44c251d3ad7b82505e268f
Awani Gupta <awanigupta396@gmail.com>
2026-09-16 00:47:29 +0530

    Add Problem 2 process and proc program

---
+++ problem2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+
+    while (1) {
+        printf("Process is running. PID = %d\n", getpid());
+        sleep(5);
+    }
+
+    return 0;
+}
+
+
