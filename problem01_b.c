81e52b07c775a8d863ed12b2a91926e73b11b676
Awani Gupta <awanigupta396@gmail.com>
2026-09-18 18:35:38 +0530

    new problem01.c updated

---
+++ problem01_b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    if (link("original.txt", "hardlink.txt") == -1)
+    {
+        perror("link");
+        return 1;
+    }
+
+    printf("Hard link created successfully.\n");
+
+    return 0;
+}
+
