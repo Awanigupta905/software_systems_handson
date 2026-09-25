732435f43e2b4f8e108b237430af39fb079ceecf
Awani Gupta <awanigupta396@gmail.com>
2026-09-25 21:31:36 +0530

    modified code

--- problem24.c
+++ problem24.c
@@ -1,43 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 
-int main()
-{
-    pid_t pid;
+int main() {
+    pid_t pid = fork();
 
-    pid = fork();
-
-    if (pid < 0)
-    {
-        perror("fork");
-        return 1;
-    }
-
-    if (pid == 0)
-    {
-        // Child process
-        printf("Child Process\n");
-        printf("Child PID: %d\n", getpid());
-        printf("Initial Parent PID: %d\n", getppid());
-
-        printf("Child is sleeping...\n");
-        sleep(10);
-
-        printf("Child PID: %d\n", getpid());
-        printf("New Parent PID: %d\n", getppid());
-        printf("Child has become an orphan process.\n");
-    }
-    else
-    {
-        // Parent process
-        printf("Parent Process\n");
-        printf("Parent PID: %d\n", getpid());
-        printf("Child PID: %d\n", pid);
-
-        printf("Parent is exiting...\n");
-        exit(0);
+    if (pid == 0) {
+        sleep(5);
+        printf("Child pid: %d, parent pid: %d\n", getpid(), getppid());
+    } else {
+        printf("Parent exiting\n");
+        _exit(0);
     }
 
     return 0;
