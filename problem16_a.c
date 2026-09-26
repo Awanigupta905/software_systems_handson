d2dc58dce0bc8b4281c94ca4e71391691cae9c24
Awani Gupta <awanigupta396@gmail.com>
2026-09-26 09:09:53 +0530

    modified

--- problem16_a.c
+++ problem16_a.c
@@ -5,59 +5,20 @@
 
 #define BUF_SIZE 256
 
-int main(int argc, char *argv[])
-{
-    int fd;
-    struct flock lock;
-    char buf[BUF_SIZE];
-    ssize_t n;
+int main(int argc, char *argv[]) {
+   int fd = open("lab16.txt", O_RDWR | O_CREAT, 0644);
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+   struct flock lock;
+   lock.l_type = F_WRLCK;
+   lock.l_whence = SEEK_SET;
+   lock.l_start = 0;
+   lock.l_len = 0;
 
-    fd = open(argv[1], O_RDWR);
-    if (fd == -1) {
-        perror("open");
-        exit(EXIT_FAILURE);
-    }
+   fcntl(fd, F_SETLK, &lock);
+   printf("Write lock acquired, sleeping 15s \n");
+   sleep(15);
 
-    /* Set up a shared read lock covering the whole file */
-    lock.l_type = F_RDLCK;
-    lock.l_whence = SEEK_SET;
-    lock.l_start = 0;
-    lock.l_len = 0;
-
-    printf("[reader] Requesting read lock...\n");
-
-    if (fcntl(fd, F_SETLKW, &lock) == -1) {
-        perror("fcntl (read lock)");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("[reader] Read lock acquired. Reading and holding for 10 seconds...\n");
-
-    n = read(fd, buf, BUF_SIZE - 1);
-
-    if (n > 0) {
-        buf[n] = '\0';
-        printf("[reader] Read: %s", buf);
-    }
-
-    sleep(10);
-
-    lock.l_type = F_UNLCK;
-
-    if (fcntl(fd, F_SETLKW, &lock) == -1) {
-        perror("fcntl (unlock)");
-    }
-
-    printf("[reader] Lock released.\n");
-
-    close(fd);
-
-    return 0;
+   close(fd);
+   return 0;
 }
 
--- problem16_b.c
+++ problem16_b.c
@@ -4,57 +4,22 @@
 #include <fcntl.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
-{
-    int fd;
+int main(int argc, char *argv[]) {
+    int fd = open("lab16.txt", O_RDWR | O_CREAT, 0644);
+
     struct flock lock;
-
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-    /* Open read-write */
-    fd = open(argv[1], O_RDWR);
-
-    if (fd == -1) {
-        perror("open");
-        exit(EXIT_FAILURE);
-    }
-
-    /* Set up an exclusive write lock covering the whole file */
-    lock.l_type = F_WRLCK;
+    lock.l_type = F_RDLCK;
     lock.l_whence = SEEK_SET;
     lock.l_start = 0;
     lock.l_len = 0;
 
-    printf("[writer] Requesting write lock...\n");
-
-    if (fcntl(fd, F_SETLKW, &lock) == -1) {
-        perror("fcntl (write lock)");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("[writer] Write lock acquired. Writing and holding for 10 seconds...\n");
-
-    const char *msg = "Written by writelock process\n";
-
-    write(fd, msg, strlen(msg));
-
-    sleep(10);
-
-    /* Release the lock */
-    lock.l_type = F_UNLCK;
-
-    if (fcntl(fd, F_SETLKW, &lock) == -1) {
-        perror("fcntl (unlock)");
-    }
-
-    printf("[writer] Lock released.\n");
+    fcntl(fd, F_SETLK, &lock);
+    printf("Read lock acquired, sleeping 15s \n");
+    sleep(15);
 
     close(fd);
-
     return 0;
 }
+
+
 
