56945fea3e83f23a549c348f486933c419495d99
Awani Gupta <awanigupta396@gmail.com>
2026-09-26 06:46:39 +0530

    modified

--- problem13.c
+++ problem13.c
@@ -1,38 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/select.h>
+#include <sys/time.h>
+#include <string.h>
 
-int main() {
+
+int main(void) {
     fd_set readfds;
-    struct timeval timeout;
-    int result;
-
-    // Clear the set
     FD_ZERO(&readfds);
+    FD_SET(0, &readfds);
 
-    // Add STDIN (file descriptor 0)
-    FD_SET(STDIN_FILENO, &readfds);
+    struct timeval tv = {10, 0};
+    int ret = select(1, &readfds, NULL, NULL, &tv);
 
-    // Set timeout to 10 seconds
-    timeout.tv_sec = 10;
-    timeout.tv_usec = 0;
-
-    printf("Waiting for input for 10 seconds...\n");
-
-    // Wait for STDIN to become ready
-    result = select(STDIN_FILENO + 1, &readfds, NULL, NULL, &timeout);
-
-    if (result == -1) {
-        perror("select");
-    }
-    else if (result == 0) {
-        printf("No data available within 10 seconds.\n");
-    }
-    else {
-        if (FD_ISSET(STDIN_FILENO, &readfds)) {
-            printf("Data is available within 10 seconds.\n");
-        }
-    }
+    if(ret > 0) printf("Data is available on stdin\n");
+    else printf("Timeout, on data received.");
 
     return 0;
 }
